0b7ebde41c289b7934817a78aa1d51453af8a45b
khojiakbarr <chinozbekov09@gmail.com>
2024-08-07 14:21:50 +0500

    upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef, useState } from "react";
 
 function App() {
+  const [stream, setStream] = useState();
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  function getCam() {
+    console.log(videoRef.current);
+
+    async function getCameraStream() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
+        setStream(stream);
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      } catch (err) {
+        console.error("Error accessing camera:", err);
+      }
+    }
+    getCameraStream();
+  }
+
+  const stopCam = () => {
+    if (stream) {
+      stream.getTracks().forEach((track) => track.stop());
+      setStream(null);
+      if (videoRef.current) {
+        videoRef.current.srcObject = null;
+      }
+    }
+  };
+
+  const takePicture = () => {
+    const context = canvasRef.current.getContext("2d");
+    context.drawImage(
+      videoRef.current,
+      0,
+      0,
+      canvasRef.current.width,
+      canvasRef.current.height
+    );
+    stopCam();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <video
+        ref={videoRef}
+        autoPlay
+        className="w-[300px] h-[300px] border-2 border-black"
+      ></video>
+      <canvas
+        ref={canvasRef}
+        className="w-[300px] h-[300px] border-2 border-black"
+      ></canvas>
+      <button
+        onClick={() => getCam()}
+        className="border-2 border-black p-[10px] rounded-lg m-[10px]"
+      >
+        Start Camera
+      </button>
+      <button
+        onClick={() => takePicture()}
+        className="border-2 border-black p-[10px] rounded-lg"
+      >
+        Take Picture
+      </button>
     </div>
   );
 }
